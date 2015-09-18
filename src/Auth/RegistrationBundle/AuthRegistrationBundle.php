1a486a5b147ae32883e9bddb9ccb94b5f01f79c2
romanveliev <Roman.Veliev@returnonintelligence.com>
2015-09-18 19:44:35 +0400

    Registration form was added.

---
+++ src/Auth/RegistrationBundle/AuthRegistrationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Auth\RegistrationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AuthRegistrationBundle extends Bundle
+{
+}
---
+++ src/Auth/RegistrationBundle/Entity/Registration.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Auth\RegistrationBundle\Entity;
+
+/**
+ * Registration
+ */
+class Registration
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Registration
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Registration
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
+
