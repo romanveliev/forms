83e179deb82e1741860cef0eb0395c64539db08b
romanveliev <Roman.Veliev@returnonintelligence.com>
2015-09-29 17:05:20 +0400

    Google captcha, validatioin were added to feedback form.

--- src/Form/RegistrationBundle/Controller/FeedbackController.php
+++ src/Form/RegistrationBundle/Controller/FeedbackController.php
@@ -4,9 +4,14 @@ namespace Form\RegistrationBundle\Controller;
 
 use Form\RegistrationBundle\Entity\Feedback;
 use Form\RegistrationBundle\Form\FeedbackType;
+use ReCaptcha\ReCaptcha;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 
 /**
@@ -15,6 +20,7 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class FeedbackController extends Controller
 {
+    private $captchaSecretKey;
     /**
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
@@ -25,17 +31,78 @@ class FeedbackController extends Controller
         $form = $this->createForm(new FeedbackType(), $comment, ['action' => $this->generateUrl('index_feedback')]);
 
         if ($request->isXMLHttpRequest()) {
-            $form->handleRequest($request);
+            $this->captchaSecretKey = $this->getParameter('captcha_private_key');
+            $data = json_decode($request->request->get('feedback'));
 
-            if ($form->isValid()) {
-                $data = $request->request->all();
+            if(isset($data[3])){
+                $recaptcha = new ReCaptcha($this->captchaSecretKey);
+                $resp = $recaptcha->verify($data[3], $_SERVER['REMOTE_ADDR']);
+                if($resp->isSuccess()){
+                    $translator = $this->get('translator');
+                    unset($data[count($data)-1]);
+
+                    $mailConstraint = new Email();
+                    $mailConstraint->message = 'invalid_email_address';
+
+                    $regexConstraint = new Regex(['pattern' => '/^[a-zA-Z]{3,16}$/']);
+                    $regexConstraint->message = 'invalid_name';
+
+                    /**
+                     * notBlank for name, email, comment
+                     */
+                    $this->checkNotBlack($data);
+
+                    /**
+                     * name validation
+                     */
+                    $errorName = $this->get('validator')->validate($data[0], $regexConstraint);
+                    if(count($errorName) != 0){
+                        $data = array('success' => false, 'error' => $translator->trans($errorName[0]->getMessage()),'code' => 0);
+                        return new JsonResponse($data);
+                    }
+
+                    /**
+                     * email validation. If it's ok, save in database.
+                     */
+                    $error = $this->get('validator')->validate($data[1], $mailConstraint);
+
+                    if(count($error) != 0){
+                        $data = array('success' => false, 'error' => $translator->trans($error[0]->getMessage()),'code' => 0);
+                        return new JsonResponse($data);
+                    }
+
+                        $session = new Session();
+                        $comment->setEmail($data[1]);
+                        $comment->setName($data[0]);
+                        $comment->setComment($data[2]);
+
+                        $em = $this->getDoctrine()->getManager();
+                        $em->persist($comment);
+                        $em->flush();
+                        $session->getFlashBag()->add('feedback', 'Thanks for feedback!!!!!!');
+
+                        $data = array('success' => 'sucess', 'code' => 1);
+                        return new JsonResponse($data);
+                }//success captcha
             }
         }
-            return $this->render('FormRegistrationBundle:Feedback:index.html.twig', [
-                'form' =>$form->createView(),
-            ]);
 
+        return $this->render('FormRegistrationBundle:Feedback:index.html.twig', [
+            'form' =>$form->createView(),
+        ]);
+    }
 
+    private function checkNotBlack($data){
+        $translator = $this->get('translator');
+        $notBlank = new NotBlank();
+        $notBlank->message = 'all_fields_should_not_be_blank';
+        foreach($data as $field){
+            $errorNotBlank = $this->get('validator')->validate($field, $notBlank);
+            if(count($errorNotBlank)!=0){
+                $data = array('success' => false, 'error' => $translator->trans($errorNotBlank[0]->getMessage()),'code' => 0);
+                return new JsonResponse($data);
+            }
+        }
     }
 
 }
--- src/Form/RegistrationBundle/Resources/public/js/app/ModalFeedback.js
+++ src/Form/RegistrationBundle/Resources/public/js/app/ModalFeedback.js
@@ -8,20 +8,36 @@ define(['jquery'],function ($) {
             $(document).ready(function(){
                 $("#feedback_submit").click(function(){
 
+                    var feedbackCaptcha = grecaptcha.getResponse(widgetId2);
+                        if(feedbackCaptcha.length == 0){
+                            $('#error_box').attr('class', 'alert alert-danger').css('display','block');
+                            return;
+                        }
+
                     var name = $('#form_registrationbundle_feedback_name').val(),
                         email = $('#form_registrationbundle_feedback_email').val(),
-                        comment = $('#form_registrationbundle_feedback_comment').val();
-                    console.log(name,email,comment);
+                        comment = $('#form_registrationbundle_feedback_comment').val(),
+                        feedback = [name, email, comment, feedbackCaptcha];
+                    console.log(name,email,comment,feedbackCaptcha);
+
+                    grecaptcha.reset(widgetId2);
 
                     $.ajax({
-                        url: '/ajax',
+                        url: '/index_feedback',
                         type: 'post',
-//                        data: {'password': JSON.stringify(password)},
+                        data: {'feedback': JSON.stringify(feedback)},
                         dataType: 'json',
                         success: function (data) {
+                            console.log(data);
                             if((typeof data) == "object"){
-
-
+                                console.log(data['code']);
+                                if(data['code'] == 0) {
+                                    $('#error_box').attr('class', 'alert alert-danger').css('display','block');
+                                    $('#errorFeedback').text('').append(data.error);
+                                }
+                                if(data['code'] == 1){
+                                    location.reload();
+                                }
                             }
                         }
                     })//ajax
